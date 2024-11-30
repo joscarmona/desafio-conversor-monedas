473aad6be2e6c21f6dffae44e1d58d9870cccc75
Jose Carmona <josshue85@gmail.com>
2024-11-30 00:28:21 -0300

    función para renderizar el resultado de la conversión de monedas y evento del button listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,35 +18,94 @@ const canvasGraficoValorMoneda10dias = document.getElementById('graficoValorMone
 /* ************************************************************************* */
 
 /* ************************************************************************* */
-/* ******* Función que permite obtener los datos del endpoint deseada ****** */
+/* ******************************** API URL ******************************** */
 /* ************************************************************************* */
-// Recibirá como parámetro el url del endpoint al que se desea conectar
-const getMonedas = async(apiEnpointUrl) => {
+// URL a la API, obtener los valores actualizados de las monedas
+// https://mindicador.cl/api/{tipo_indicador}: entrega los valores del último mes del indicador consultado
+const apiUrl = "https://mindicador.cl/api/"
+/* ************************************************************************* */
+
+/* ************************************************************************* */
+/* ***************** Obtener los datos de la API o endpoint **************** */
+/* ************************************************************************* */
+// Recibirá como parámetro el url de la API o del endpoint
+const getMonedas = async(apiEndpointUrl) => {
     try {
         // Se obtienen datos desde el endpoint / API
-        // const url = apiEnpointUrl
-        const response = await fetch(apiEnpointUrl)
+        const response = await fetch(apiEndpointUrl)
         // console.log(response)
 
         // Se parsea a JSON
         const data = await response.json()
         // console.log(data)
+
+        return data
     } catch(error) {
-        alert("No fue posible cargar los datos,  por favor intente nuevamente")
+        console.log(error.message)
+        // alert("No fue posible cargar los datos,  por favor intente nuevamente")
+        // Se muestra un mensaje indicando que no fue posible conectar con la API o con el endpoint según sea el caso
+        pResultadoConversion.textContent = `No fue posible cargar los datos. Por favor intente nuevamente!`
     }
 }
-
-// API/endpoint que se desea consultar
-// const endpointUrl = "https://mindicador.cl/api/"
-// const endpointUrl = "https://mindicador.cl/api/dolar"
-// const endpointUrl = "https://mindicador.cl/api/euro/04-11-2024"
-// Llamado de la función getMonedas()
-// getMonedas(endpointUrl)
 /* ************************************************************************* */
 
 /* ************************************************************************* */
-/* ********** Función que realiza el cálculo del cambio de monedas ********* */
+/* **************** Realizar el cálculo del cambio de monedas *************** */
+/* ******************* Conversión de CLP a otras monedas ******************* */
 /* ******************* y muestra en el DOM el resultado ******************** */
 /* ************************************************************************* */
+// montoAConvertir: parámetro que indica el monto ingresado a través del <INPUT>
+// valorMonedaAConvertir: parámetro que indica el valor actual de la moneda a la cual se desea convertir
+// to.fixed(): el método toFixed() redondea la cadena a un número específico de decimales.
+// const getResultadoConversionMoneda = (montoAConvertir, valorMonedaAConvertir) => {
+//     // Operación de la conversión
+//     const resultadoConversionMoneda = montoAConvertir / valorMonedaAConvertir
+//     // console.log(resultadoConversionMoneda)
+
+//     return resultadoConversionMoneda
+// }
 
 /* ************************************************************************* */
+
+/* **************************************************************************** */
+/* ************ Renderizar el resultado de la conversión de monedas *********** */
+/* **************************************************************************** */
+// const renderResultado = async(monedaAConvertir) => {
+const renderResultado = async(montoAConvertir, monedaAConvertir) => {
+    // Obtener los datos de las monedas desde la API
+    const monedasData =  await getMonedas(apiUrl)
+
+    // Convertir CLP a la moneda seleccionada
+    // const resultado = getResultadoConversionMoneda(Number(inputMontoEnClp.value), monedasData[monedaAConvertir].valor)
+    const resultado = montoAConvertir / monedasData[monedaAConvertir].valor
+    pResultadoConversion.textContent = `Resultado: $${resultado.toFixed(2)}`
+}
+/* **************************************************************************** */
+
+
+/* **************************************************************************** */
+/* ******* Renderizar gráfico con los valores de la moneda seleccionada ******* */
+/* *********************** Durante los últimos 10 días ************************ */
+/* **************************************************************************** */
+
+/* **************************************************************************** */
+
+
+/* **************************************************************************** */
+/* *************************** EVENTO del <BUTTON> **************************** */
+/* **************************************************************************** */
+buttonConvertirGraficar.addEventListener("click", () =>{
+    // Además de la validación que tiene por defecto el INPUT para ingresar solo números, igualmente se chequea
+    // a continuación que el valor a ingresar sea a partir del número 1 y solo valores positivos
+    if(Number(inputMontoEnClp.value) > 0 && Number.isInteger(Number(inputMontoEnClp.value)) && selectMonedaAConvertir.value != "") {
+        // alert("monto ingresado válido")
+        // Mostrar resultado de la conversión de CLP a la moneda seleccionada
+        // renderResultado(selectMonedaAConvertir.value)
+        renderResultado(Number(inputMontoEnClp.value), selectMonedaAConvertir.value)
+
+    } else{
+        alert("por favor ingrese un monto válido y/o seleccione una moneda a convertir")
+        pResultadoConversion.textContent = `...`
+        inputMontoEnClp.value = ""
+    }
+})
